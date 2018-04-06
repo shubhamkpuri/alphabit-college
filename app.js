93c2bbd15d22e718c3eed80d062867c90e46de4e
Shubham Kumar Puri <shubhampuri8169@gmail.com>
2018-04-06 18:46:52 +0530

    Little changes

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ var app = express();
 mongoose.connect("mongodb://localhost/alphbit");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// User Schemas 
+// User Schemas
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: String,
+  password: String
 });
 UserSchema.plugin(passportLocalMongoose);
 var User = mongoose.model("User", UserSchema);
@@ -25,9 +27,9 @@ var User = mongoose.model("User", UserSchema);
 
 // Passport Coniguration
 app.use(session({
-    secret: "ShubhamKumarPuri",
-    resave: false,
-    saveUninitialized: false
+  secret: "ShubhamKumarPuri",
+  resave: false,
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,35 +39,39 @@ passport.deserializeUser(User.deserializeUser());
 
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
+  res.locals.currentUser = req.user;
 
-    next();
+  next();
 });
 
 //Project Schema
 var ProjectSchema = new mongoose.Schema({
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     },
-    title: String,
-    type: String,
-    technology: String,
-    shortDesc: String,
-    body: String,
-    posted: {
-        type: Date,
-        default: Date.now
-    }
+    username: String
+  },
+  title: String,
+  type: String,
+  technology: String,
+  shortDesc: String,
+  body: String,
+  likes: {
+    type: Number,
+    default: 0
+  },
+  posted: {
+    type: Date,
+    default: Date.now
+  }
 
 });
 var Project = mongoose.model("Projects", ProjectSchema);
@@ -85,81 +91,85 @@ var Project = mongoose.model("Projects", ProjectSchema);
 
 app.get("/", (req, res) => {
 
-    console.log(req.user);
-    Project.find({}).sort({ posted: -1 }).limit(4).exec((err, Projects) => {
+  console.log(req.user);
+  Project.find({}).sort({
+    posted: -1
+  }).limit(4).exec((err, Projects) => {
 
-        if (err) {
-            console.log("ERROR!");
-        } else {
-            res.render("home", {
-                Projects: Projects,
-                title: "Alphabit"
-            });
-        }
-    });
+    if (err) {
+      console.log("ERROR!");
+    } else {
+      res.render("home", {
+        Projects: Projects,
+        title: "Alphabit"
+      });
+    }
+  });
 
 
 });
 
 app.get("/ques", (req, res) => {
-    res.render("ques.ejs", {
-        title: 'Q/A '
-    });
+  res.render("ques.ejs", {
+    title: 'Q/A '
+  });
 });
 
 app.post("/ques/new", (req, res) => {
-    res.render("ques");
+  res.render("ques");
 });
 
 // projects
 
 app.get('/projects/new', isLoggedIn, (req, res) => {
-    res.render("projects/new.ejs", {
-        title: "New Project"
-    });
+  res.render("projects/new.ejs", {
+    title: "New Project"
+  });
 });
 
 app.post("/projects", (req, res) => {
-    req.body.project.author = req.user;
-    console.log(req.body.project.author);
-    Project.create(req.body.project, (err, newProject) => {
-        if (err) {
-            res.render("new");
-        } else {
+  req.body.project.author = req.user;
+  console.log(req.body.project.author);
+  Project.create(req.body.project, (err, newProject) => {
+    if (err) {
+      res.render("new");
+    } else {
 
-            newProject.save();
-            //redirect to index
-            res.redirect("/");
-        }
-    })
+      newProject.save();
+      //redirect to index
+      res.redirect("/");
+    }
+  })
 });
 
 app.get('/projects/:id', (req, res) => {
-    Project.findById(req.params.id, (err, foundProject) => {
-        if (err) {
-            console.log("problem");
-            // res.redirect("/projects");
-        } else {
-            console.log(foundProject);
-            res.render("projects/show.ejs", {
-                project: foundProject,
-                title: "Project"
-            });
-        }
-    });
+  Project.findById(req.params.id, (err, foundProject) => {
+    if (err) {
+      console.log("problem");
+      // res.redirect("/projects");
+    } else {
+      console.log(foundProject);
+      res.render("projects/show.ejs", {
+        project: foundProject,
+        title: "Project"
+      });
+    }
+  });
 });
 app.get('/projects', (req, res) => {
-    Project.find({}).sort({ posted: -1 }).exec((err, Projects) => {
+  Project.find({}).sort({
+    posted: -1
+  }).exec((err, Projects) => {
 
-        if (err) {
-            console.log("ERROR!");
-        } else {
-            res.render("projects/index.ejs", {
-                Projects: Projects,
-                title: "All Projects"
-            });
-        }
-    });
+    if (err) {
+      console.log("ERROR!");
+    } else {
+      res.render("projects/index.ejs", {
+        Projects: Projects,
+        title: "All Projects"
+      });
+    }
+  });
 });
 
 //=======================
@@ -169,48 +179,50 @@ app.get('/projects', (req, res) => {
 //show register form
 
 app.get("/register", (req, res) => {
-    res.render("register", {
-        title: "Register",
-        msg: "5"
-    });
+  res.render("register", {
+    title: "Register",
+    msg: "5"
+  });
 });
 //handle sign up logic
 app.post("/register", (req, res) => {
-    var newUser = new User({ username: req.body.username });
-    User.register(newUser, req.body.password, (err, user) => {
-        if (err) {
-            console.log(err);
-            res.render("register", {
-                title: "Register",
-                msg: err
-            });
-        }
-        passport.authenticate("local")(req, res, () => {
-            res.redirect("/");
-        });
+  var newUser = new User({
+    username: req.body.username
+  });
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.render("register", {
+        title: "Register",
+        msg: err
+      });
+    }
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/");
     });
+  });
 });
 
 // show login form
 app.get("/login", (req, res) => {
-    res.render("login", {
-        title: "Register",
-    });
+  res.render("login", {
+    title: "Register",
+  });
 });
 app.post("/login", passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login"
-    }),
-    (req, res) => {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  }),
+  (req, res) => {
 
-    });
+  });
 //logout
 app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
+  req.logout();
+  res.redirect("/");
 });
 
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is listening on 8000`);
+  console.log(`Server is listening on 8000`);
 })
