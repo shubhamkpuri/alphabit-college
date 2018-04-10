62c6f1f7929733382a7963654294015a045916b0
Shubham Kumar Puri <shubhampuri8169@gmail.com>
2018-04-10 18:57:49 +0530

    Added viewport

--- public/JavaScript/home.js
+++ public/JavaScript/home.js
@@ -3,11 +3,32 @@ function formvalidate(number, idname, elementname){
     console.log(v);
     alert(v);
 }
-//for hiding navbar
+document.getElementById("togglebtn").addEventListener("click", () => {
+
+    document.getElementById("navbar-hide").classList.toggle("navbar-hide");
+    document.getElementById("navbar-hide").classList.remove("removeanimation");
+    document.getElementById("navbar-hide").classList.add("addanimation");
+
+
+});
+//ECS key for getting out of navigation
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        cancelfun();
+    }
+};
 var cancelfun = () => {
-    document.getElementById("navbar-hide").classList.add("navbar-hide");
+    document.getElementById("navbar-hide").classList.add("removeanimation");
+ document.getElementById("navbar-hide").classList.remove("addanimation");
+ setTimeout(alertFunc, 280);
+
+
 }
 
+function alertFunc() {
+  document.getElementById("navbar-hide").classList.toggle("navbar-hide");
+}
 
 //Full page .js
 
